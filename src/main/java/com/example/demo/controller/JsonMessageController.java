b2b2a97fda1ffffecf5acbae2b3d22437a003440
duydo8 <duydogch123456@gmail.com>
2023-03-20 16:24:56 +0700

    commit demo

---
+++ src/main/java/com/example/demo/controller/JsonMessageController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.kafka.JsonKafkaProducer;
+import com.example.demo.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/kafka/p1")
+@RestController
+public class JsonMessageController {
+    private JsonKafkaProducer jsonKafkaProducer;
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer){
+        this.jsonKafkaProducer=jsonKafkaProducer;
+    }
+    @PostMapping("/publish")
+    public ResponseEntity<?> publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("JsonMessage sent to topic");
+    }
+
+}
---
+++ src/main/java/com/example/demo/kafka/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.example.demo.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private final static Logger logger= LoggerFactory.getLogger(KafkaProducer.class);
+    @KafkaListener(topics = "topicDemo",groupId = "myGroup")
+    public void consume(String message){
+        logger.info(String.format("message received -> %s",message));
+    }
+}
